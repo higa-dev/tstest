5c1299470e8a86bb96c8765e570eb46f33f4b3eb
higa-dev <higadev1140@gmail.com>
2023-03-01 16:02:54 +0900

    page

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+"use strict";
+class Hoge {
+    hoge(params) {
+        return params * 2;
+    }
+}
+/// <reference path="hoge.ts"/>
+window.onload = function () {
+    const target = document.getElementById('target');
+    if (target) {
+        target.innerHTML = String(new Hoge().hoge(100));
+    }
+};
+//# sourceMappingURL=main.js.map
